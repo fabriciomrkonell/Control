8c23e334b2db2b640018655331ea7988af0a814b
Fabricio <fabricio_konell@hotmail.com>
2013-12-30 17:21:36 -0200

    Jquery-ui, Angular... 2

--- assets/js/angular/angular-sliders.js
+++ assets/js/angular/angular-sliders.js
@@ -1,3 +1,16 @@
 var app = angular.module('myApp', ['ui.slider']);
 
-app.controller('myAppController', function($scope) {});
+app.controller('myAppController', function($scope) {
+
+	$scope.iconSliders = "icon-light-down";
+
+	$scope.atualizarSliders = function(){
+		if($scope.lightModel < 33){
+			$scope.iconSliders = "icon-light-down";
+		}else if($scope.lightModel > 33 && $scope.lightModel < 66){
+			$scope.iconSliders = "icon-adjust";
+		}else{
+			$scope.iconSliders = "icon-light-up";
+		}
+	}
+});
